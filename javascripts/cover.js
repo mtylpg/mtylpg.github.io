20c111489be7297a2f6b0d09c8a1946c3e9ba547
MattyLapage <matthewclepage@gmail.com>
2014-02-15 22:16:53 -0500

    YET ANOTHER cover but this one is nice and pretty like

--- javascripts/cover.js
+++ javascripts/cover.js
@@ -1,14 +1,49 @@
 $(document).ready(function() {
 
-    $('.oldLink').on("mouseenter", function() {
-        $('#old').css('color','#007dfb');
-        $('#old').animate({"margin-top":"-.9em"}, 100, "linear");
-        $('#bad').animate({"margin-top":"0em"}, 100, "linear");
-    });
-    $('.oldContainer').on("mouseleave", function() {
-        $('#old').css('color','#444');
-        $('#old').animate({"margin-top":"0em"}, 100, "linear");
-        $('#bad').animate({"margin-top":".9em"}, 100, "linear");
+
+    $('.navLink').each(function () {
+        $(this).on("click", function() { 
+            targetHeight = $('#'+$(this).attr("link")).offset().top-50;
+            $('body').animate({ scrollTop: targetHeight}, 150);
+        });
     });
 
+    var navContainer = $('#navContainer');
+    var navFiller = $('#navFiller');
+
+    var skillsbg = $('#skillsbg');
+    var contactbg = $('#contactbg');
+    var bg1 = $('#bg1');
+    var bg2 = $('#bg2');
+
+
+    $(window).on("scroll", function() {
+        offset = $("body").scrollTop();
+        
+        if (offset > 335){
+            navContainer.addClass("floatNav");
+            navFiller.addClass("navFill");
+        }
+        else {
+            navContainer.removeClass("floatNav");
+            navFiller.removeClass("navFill");
+        }
+
+        setBackgrounds(offset);
+    });
+
+    function setBackgrounds(yPos){
+        skillsbg.css(
+            'background-position','-190px -'+
+                (yPos*0.4+0)+'px');
+        contactbg.css(
+            'background-position','-0px -'+
+                (yPos*0.4-300)+'px');
+        bg1.css(
+            'background-position','0px -'+
+                (yPos*0.7+120)+'px');
+        bg2.css(
+            'background-position','100% '+
+                (yPos*0.7)+'px');
+    }
 });
