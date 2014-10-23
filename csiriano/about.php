bc5bda1f5ed7a9031319395a1bbca00ed842dc42
Matty <matthewclepage@gmail.com>
2014-10-23 09:42:12 -0400

    Let's just store this here for a bit

---
+++ csiriano/about.php
@@ -0,0 +1,20 @@
+<font size="14" face="georgia">
+<a href="http://www.christiansiriano.com/mobile"><img src="http://www.christiansiriano.com/mobile/mobilelogo.jpg"></a><br>
+<br><FONT COLOR="FF0000"><b>ABOUT</b></FONT><br><br>
+Following his studies in London under Vivienne Westwood and Alexander McQueen, Christian Siriano launched his eponymous collection in 2008. The Christian Siriano collection is now shown each season at New York Fashion Week, and presented in New York and Paris to retailers. Known for whimsical and show-stopping design – from fantasy evening gowns and cocktail dresses, to tailored sportswear, to intricately detailed shoes and accessories – the collection can be found at department stores and specialty boutiques around the globe. In 2012 the first flagship Christian Siriano store opened on Elizabeth Street in New York City, and in 2013 he was inducted as a member of the Council of Fashion Designers of America (CFDA).<br>
+<br>
+Dubbed "the new king of old-school glamour" by <i>Elle</i>, his designs have appeared in countless magazines and have been worn by the world's leading ladies, from stars of film (Sarah Jessica Parker, Jennifer Lopez, Shailene Woodley, Elizabeth Olsen, Emily Blunt, Chloe Grace Moretz, Rosario Dawson, Christina Ricci) to icons of music (Rihanna, Lady Gaga, Taylor Swift, Fergie, Nicki Minaj, Pink, Carrie Underwood) to stars of television (Lea Michele, Christina Hendricks, Heidi Klum, Allison Williams, Debra Messing, Kristin Davis) and darlings of fashion (Victoria Beckham, Alexa Chung, Dita Von Teese, Kate Upton, Olivia Palermo, Coco Rocha), to name just a few. Oprah Winfrey calls his designs “works of art” and Tim Gunn calls him “a prodigy” and “the next great American fashion designer.”<br>
+<br>
+Christian Siriano also designs a bridal collection for Nordstrom, as well as four seasonal collections of shoes and handbags for Payless ShoeSource, an ongoing guest designer collaboration since 2009. Other design partnerships have included a makeup line for Victoria’s Secret, and clothing and accessories for HSN, Disney, Puma, LG, Starbucks, Spiegel, and others. He is the youngest person to ever appear on the Crain's “40 Under 40” list, and he co-founded accessories e-commerce retailer “Send the Trend” and served as Chief Curator until its acquisition by QVC in 2012. Christian Siriano fragrance and eyewear will launch in 2014.<br>
+</font>
+<br><br><br><font size="14" face="helvetica"><b><a href="http://www.christiansiriano.com/mobile/index.php">HOME</a><br>
+<a href="http://www.christiansiriano.com/mobile/collections.php">COLLECTIONS</a><br>
+<a href="http://www.christiansiriano.com/mobile/about.php">ABOUT</a><br>
+<a href="http://www.christiansiriano.com/mobile/press.php">PRESS</a><br>
+<a href="http://www.christiansiriano.com/mobile/seenon.php">SEEN ON</a><br>
+<a href="http://store.christiansiriano.com/products">ONLINE STORE</a><br>
+<a href="http://www.christiansiriano.com/mobile/retailers.php">RETAILERS</a><br>
+<a href="http://www.christiansiriano.com/mobile/contact.php">CONTACT</a></b></br></font>
+<br>
+<br><br><br><font size="14" face="helvetica"><a href="http://twitter.com/csiriano">Twitter</a> | <a href="http://facebook.com/christiansiriano">Facebook</a> | <a href="http://instagram.com/csiriano">Instagram</a> | <a href="http://csiriano.tumblr.com">Tumblr</a><br>
+&#64; 2008-2014 Christian Siriano Holdings LLC<br></font>
---
+++ csiriano/retailers.php
@@ -0,0 +1,226 @@
+<font size="14" face="georgia">
+<a href="http://www.christiansiriano.com/mobile"><img src="http://www.christiansiriano.com/mobile/mobilelogo.jpg"></a><br>
+<br><FONT COLOR="FF0000"><b>RETAILERS</b></FONT><br>
+Where to shop Christian Siriano. Organized by location: United States, International, and Web/Online.<br><br>
+<br><FONT COLOR="3B9C9C">ALABAMA<br></FONT>
+<br>
+<a href="http://www.gusmayer.com/">Gus Mayer<br>
+Birmingham, AL</a><br>
+<br>
+<br><FONT COLOR="3B9C9C">ARIZONA<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=29/SC">Neiman Marcus<br>
+Scottsdale, AZ<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">CALIFORNIA<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=10/LA">Neiman Marcus<br>
+Beverly Hills, CA<br></a>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=24/PA">Neiman Marcus<br>
+Palo Alto, CA<br></a>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=12/SF">Neiman Marcus<br>
+San Francisco, CA<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">CONNECTICUT<br></FONT>
+<br>
+<a href="http://larmoirenewcanaan.com/">L'Armoire<br>
+New Canaan, CT<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">FLORIDA<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=05/BH">Neiman Marcus<br>
+Bal Harbour, FL<br></a>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=32/PB">Neiman Marcus<br>
+Palm Beach, FL<br></a>
+<br>
+<a href="http://www.johndemedeiros.com/">John De Medeiros<br>
+Palm Beach, FL<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">GEORGIA<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=06/AT">Neiman Marcus<br>
+Atlanta, GA<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">ILLINOIS<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=19/MA">Neiman Marcus<br>
+Chicago, IL<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">KENTUCKY<br></FONT>
+<br>
+<a href="http://glasscockinc.com/">Glasscock Boutique<br>
+Louisville, KY<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">MARYLAND<br></FONT>
+<br>
+<a href="https://www.facebook.com/TheBridalSalonatSaksJandel">Saks Jandel<br>
+Chevy Chase, MD<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">MASSACHUSSETTS<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=20/BN">Neiman Marcus<br>
+Boston, MA<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">NEBRASKA<br></FONT>
+<br>
+<a href="http://www.shopshela.com/">She.La<br>
+Omaha, NE<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">NEW JERSEY<br></FONT>
+<br>
+<a href="http://botteganj.com/index.html">Bottega<br>
+Lakewood, NJ<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">NEW YORK<br></FONT>
+<br>
+<a href="http://www.yelp.com/biz/christian-siriano-new-york">Christian Siriano<br>
+252 Elizabeth St, New York City<br></a>
+<br>
+<a href="http://www.estisonly.com/">Esti's<br>
+Brooklyn, NY<br></a>
+<br>
+<a href="http://www.pinklabelny.com/">Pink Label<br>
+Brooklyn, NY<br></a>
+<br>
+<a href="http://www.vivaldi-ny.com/">Vivaldi Boutique<br>
+New York, NY<br></a>
+<br>
+<a href="http://sharisplace.com/">Shari's Place<br>
+Greenvale, NY<br></a>
+<br>
+<a href="http://mjdenzer.com/mjdenzer/">Mary Jane Denzer<br>
+White Plains, NY<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">SOUTH CAROLINA<br></FONT>
+<br>
+<a href="http://gwynns.com/">Gwynn's<br>
+Mount Pleasant, SC<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">TENNESSEE<br></FONT>
+<br>
+<a href="http://www.gusmayer.com/">Gus Mayer<br>
+Nashville, TN<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">TEXAS<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=04/HO">Neiman Marcus<br>
+Houston, TX<br></a>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=02/NP">Neiman Marcus<br>
+Dallas, TX (Northpark)<br></a>
+<br>
+<a href="https://www.stanleykorshak.com/">Stanley Korshak<br>
+Dallas, TX<br></a>
+<br>
+<a href="http://joebrand.com/gallery/">Joe Brand<br>
+Laredo, TX<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">WASHINGTON, DC<br></FONT>
+<br>
+<a href="http://www.neimanmarcus.com/stores/store.jsp?storeId=08/WA">Neiman Marcus<br>
+Washington, DC<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">INTERNATIONAL<br></FONT>
+<br>
+<a href="http://www.on-motcomb.com/">ON Motcomb<br>
+London, England<br></a>
+<br>
+<a href="http://www.mycouturecorner.com/">My Couture Corner<br>
+Paris, France<br></a>
+<br>
+<a href="http://www.boutique-no7.com/">Boutique No. 7<br>
+Moscow, Russia<br></a>
+<br>
+<a href="https://www.facebook.com/pages/POIS/108626512522383">Pois<br>
+Singapore<br></a>
+<br>
+<a href="http://www.blksheep.com/">Blk Sheep<br>
+Toronto, Ontario, Canada<br></a>
+<br>
+<a href="http://www.hugonicholson.com/">Hugo Nicholson<br>
+Toronto, Ontario, Canada<br></a>
+<br>
+<a href="http://www.harveynichols.com/stores/stores-abroad">Harvey Nichols<br>
+Kuwait<br></a>
+<br>
+<a href="http://alothman-fashion.com/">Alothman<br>
+Salmiya, Kuwait<br></a>
+<br>
+<a href="http://www.alboushiya.com/">Al Boushiya<br>
+Abu Dhabi, UAE<br></a>
+<br>
+<a href="http://bysymphony.com/">By Symphony<br>
+Dubai, UAE<br></a>
+<br>
+<a href="http://www.harveynichols.com/stores/stores-abroad">Harvey Nichols<br>
+Dubai, UAE<br></a>
+<br>
+<a href="http://alothman-fashion.com/">Alothman<br>
+Manama, Bahrain<br></a>
+<br>
+<a href="http://www.arabianapparel.net/">Arabian Apparel<br>
+KSA<br></a>
+<br>
+<a href="http://www.blankboutique.net/">Blank Boutique<br>
+KSA<br></a>
+<br>
+<a href="https://www.facebook.com/pages/Casual-Smart/524834820885967">Casual Smart<br>
+Al Khobar, KSA<br></a>
+<br>
+<a href="http://www.mimosa.com.sa/">Mimosa<br>
+KSA<br></a>
+<br>
+<a href="http://www.mahat.com.sa/index_1.htm?page=contactus.php">Mahat Boutique<br>
+Jeddah, KSA<br></a>
+<br>
+<a href="http://www.sjtc.com.sa/">Saudi Jawahir Trading Co<br>
+Riyadh, KSA<br></a>
+<br>
+<a href="http://www.theclosetqatar.com/">The Closet Qatar<br>
+Doha, State of Qatar<br></a>
+<br>
+<br><FONT COLOR="3B9C9C">WEB/ONLINE<br></FONT>
+<br>
+<a href="http://store.christiansiriano.com/products">Christian Siriano Online Store<br>
+store.christiansiriano.com<br></a>
+<br>
+<a href="http://www.neimanmarcus.com/search.jsp?N=0&Ntt=christian+siriano">Neiman Marcus<br>
+www.neimanmarcus.com<br></a>
+<br>
+<a href="http://shop.nordstrom.com/c/christian-siriano-lookbook">Nordstrom (Bridal)<br>
+www.nordstrom.com<br></a>
+<br>
+<a href="https://www.stanleykorshak.com/designer/Christian-Siriano/349">Stanley Korshak<br>
+www.stanleykorshak.com<br></a>
+<br>
+<a href="http://www.polyvore.com/cgi/shop?query=christian+siriano">Polyvore<br>
+www.polyvore.com<br></a>
+<br>
+<a href="http://www.farfetch.com/shopping/women/christian-siriano/items.aspx">Farfetch<br>
+www.farfetch.com<br></a>
+<br>
+<a href="https://www.renttherunway.com/designer_detail/christiansiriano">Rent the Runway<br>
+www.renttherunway.com<br></a>
+<br>
+<a href="http://rentfrockrepeat.com/t/designers/christian-siriano">Rent Frock Repeat<br>
+www.rentfrockrepeat.com<br></a>
+<br>
+<a href="http://styleinabox.eu/en/designers/christian-siriano">Style in A Box<br>
+www.styleinabox.eu<br></a>
+<br>
+<a href="http://www.myhabit.com/">MyHabit<br>
+www.myhabit.com<br></a><br></font>
+<br><br><br><font size="14" face="helvetica"><b><a href="http://www.christiansiriano.com/mobile/index.php">HOME</a><br>
+<a href="http://www.christiansiriano.com/mobile/collections.php">COLLECTIONS</a><br>
+<a href="http://www.christiansiriano.com/mobile/about.php">ABOUT</a><br>
+<a href="http://www.christiansiriano.com/mobile/press.php">PRESS</a><br>
+<a href="http://www.christiansiriano.com/mobile/seenon.php">SEEN ON</a><br>
+<a href="http://store.christiansiriano.com/products">ONLINE STORE</a><br>
+<a href="http://www.christiansiriano.com/mobile/retailers.php">RETAILERS</a><br>
+<a href="http://www.christiansiriano.com/mobile/contact.php">CONTACT</a></b></br></font>
+<br>
+<br><br><br><font size="14" face="helvetica"><a href="http://twitter.com/csiriano">Twitter</a> | <a href="http://facebook.com/christiansiriano">Facebook</a> | <a href="http://instagram.com/csiriano">Instagram</a> | <a href="http://csiriano.tumblr.com">Tumblr</a><br>
+&#64; 2008-2014 Christian Siriano Holdings LLC<br></font>
