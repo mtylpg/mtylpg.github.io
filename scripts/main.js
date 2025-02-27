f1d6750f2530e975cb4763f0797fce231513c75d
Matt Lepage <matthewclepage@gmail.com>
2025-02-27 13:45:19 -0500

    First pass at animated wave background
    Known issues on short screens etc, still thinking about a broader fade-in-fade-out sulution

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
 console.log('This would be the main JS file.');
 
 
-stars = document.getElementById("stars");
-starCanvas = document.getElementsByClassName("stars-container")[0];
-starCanvasArea = starCanvas.getBoundingClientRect();
-headerContainer = document.getElementsByClassName("line-offset")[0];
+// stars = document.getElementById("stars");
+// starCanvas = document.getElementsByClassName("stars-container")[0];
+// starCanvasArea = starCanvas.getBoundingClientRect();
+// headerContainer = document.getElementsByClassName("line-offset")[0];
 
 
 
 document.addEventListener("DOMContentLoaded", (event) => {
     let mm = gsap.matchMedia();
+
+    gsap.to('.animated--bg', {
+        transform: 'translateY(-100vh)', 
+        // rotation: -120,
+        scrollTrigger: {
+          trigger: 'h2',
+          start: 'top bottom',
+          end: 'top top',
+          scrub: true
+        }
+      });
+
 
     // large-screen animations
     // mm.add("(min-width: 800px)", () => {
@@ -99,110 +111,88 @@ document.addEventListener("DOMContentLoaded", (event) => {
     //     y: -120,
     // });
    });
-  
+
+// starArray = [];
+// initial_maxstars = 6;
+// initial_spawnRate = 1500;
+// initial_waveRate = 5000;
+
+// if (starCanvasArea.width<1000)
+// {
+//     maxStars = initial_maxstars/3;
+//     spawnRate = initial_spawnRate*2;
+//     waveRate = initial_waveRate*2;
+
+// } else {
+//     maxStars = initial_maxstars;
+//     spawnRate = initial_spawnRate;
+//     waveRate = initial_waveRate;
+// }
 
 
+// console.log(stars);
+// function getRandomInt(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor((Math.random()+.01) * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+//   }  
 
+// function spawnStar(x, y, rotation) {
+//     let star_container = document.createElement('span')
+//     star_container.classList.add("star-container");
 
+//     star_container.setAttribute("style", "top:"+y+"px; left:"+x+"px; rotate:"+rotation+"deg;");
 
+//     star_wipe_container = star_container.cloneNode(true);
 
+//     let img = document.createElement('img');
+//     img.classList.add("star-animation");
 
+//     img2 = img.cloneNode(true);
+//     img2.classList.add("wipe");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-starArray = [];
-initial_maxstars = 6;
-initial_spawnRate = 1500;
-initial_waveRate = 5000;
-
-if (starCanvasArea.width<1000)
-{
-    maxStars = initial_maxstars/3;
-    spawnRate = initial_spawnRate*2;
-    waveRate = initial_waveRate*2;
-
-} else {
-    maxStars = initial_maxstars;
-    spawnRate = initial_spawnRate;
-    waveRate = initial_waveRate;
-}
-
-
-console.log(stars);
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor((Math.random()+.01) * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }  
-
-function spawnStar(x, y, rotation) {
-    let star_container = document.createElement('span')
-    star_container.classList.add("star-container");
-
-    star_container.setAttribute("style", "top:"+y+"px; left:"+x+"px; rotate:"+rotation+"deg;");
-
-    star_wipe_container = star_container.cloneNode(true);
-
-    let img = document.createElement('img');
-    img.classList.add("star-animation");
-
-    img2 = img.cloneNode(true);
-    img2.classList.add("wipe");
-
-    img.src = '../images/textures/star-blue.svg';
-    img2.src = '../images/textures/star-white.svg';
+//     img.src = '../images/textures/star-blue.svg';
+//     img2.src = '../images/textures/star-white.svg';
     
-    star_container.appendChild(img);
-    star_wipe_container.appendChild(img2);
+//     star_container.appendChild(img);
+//     star_wipe_container.appendChild(img2);
 
-    stars.appendChild(star_container);
-    stars.appendChild(star_wipe_container);
-    starArray.push(star_container, star_wipe_container);
-}
+//     stars.appendChild(star_container);
+//     stars.appendChild(star_wipe_container);
+//     starArray.push(star_container, star_wipe_container);
+// }
 
-function spawnStars(count, spawnRate) {
-    return;
-    lastRandom = false;
-    lastSpawn = 0
-    nextSpawn = 0;
-    for (let i = 0; i < count; i++) {
-        if (lastRandom){
-            nextSpawn = spawnRate - lastSpawn;
-            lastRandom = false;
-        } else{
-            nextSpawn = 500;
-            lastRandom = true;
-        }
-        // console.log(starArray);
-        if (starArray.length > maxStars)
-        {
-            starArray[0].parentNode.removeChild(starArray[0]);
-            starArray.shift();
-            starArray[0].parentNode.removeChild(starArray[0]);
-            starArray.shift();
-        }
-        setTimeout(function(){
-            spawnStar (
-                getRandomInt(starCanvasArea.left+400,starCanvasArea.left+starCanvasArea.width-400),getRandomInt(starCanvasArea.top-80, starCanvasArea.top+starCanvasArea.height-400),getRandomInt(0,180))
-        }, 1000);
-        lastSpawn = nextSpawn;
-    }
-}
+// function spawnStars(count, spawnRate) {
+//     return;
+//     lastRandom = false;
+//     lastSpawn = 0
+//     nextSpawn = 0;
+//     for (let i = 0; i < count; i++) {
+//         if (lastRandom){
+//             nextSpawn = spawnRate - lastSpawn;
+//             lastRandom = false;
+//         } else{
+//             nextSpawn = 500;
+//             lastRandom = true;
+//         }
+//         // console.log(starArray);
+//         if (starArray.length > maxStars)
+//         {
+//             starArray[0].parentNode.removeChild(starArray[0]);
+//             starArray.shift();
+//             starArray[0].parentNode.removeChild(starArray[0]);
+//             starArray.shift();
+//         }
+//         setTimeout(function(){
+//             spawnStar (
+//                 getRandomInt(starCanvasArea.left+400,starCanvasArea.left+starCanvasArea.width-400),getRandomInt(starCanvasArea.top-80, starCanvasArea.top+starCanvasArea.height-400),getRandomInt(0,180))
+//         }, 1000);
+//         lastSpawn = nextSpawn;
+//     }
+// }
 
-spawnStars(1, 1);
-setInterval(function() {spawnStars(1, spawnRate)}, 2000);
+// spawnStars(1, 1);
+// setInterval(function() {spawnStars(1, spawnRate)}, 2000);
 
 // addEventListener("resize", (event) => {
 //     console.log(starCanvasArea);
